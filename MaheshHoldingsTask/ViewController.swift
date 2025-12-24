fe6dfa9edd70909a4327d6ac7ff58e4d6a2430fc
Mahesh Bijapur <glo88070@adobe.com>
2025-12-24 16:30:05 +0530

    Initial project setup with UIKit and unit tests

---
+++ MaheshHoldingsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaheshHoldingsTask
+//
+//  Created by Mahesh on 12/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
